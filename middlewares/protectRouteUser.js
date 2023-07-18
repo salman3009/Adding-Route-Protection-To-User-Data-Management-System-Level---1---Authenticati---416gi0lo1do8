8ed47894dbaf2f7c974b233f777361706b577250
salman3009 <salman444g@gmail.com>
2023-07-18 22:20:56 +0530

    code

--- middlewares/protectRouteUser.js
+++ middlewares/protectRouteUser.js
@@ -15,8 +15,23 @@ Resource doesn't belong to the user: { message: 'Access Denied', status: 'error'
 */
 
 const protectUserRoutes = (req, res, next) => {
+    const token = req.headers.authorization;
+    if(!token){
+        return res.status(401).json({
+            message:'Authentication failed: Missing token.',
+            status:'error'
+        })
+    }
     try {
-        //Write your code here
+       const decodedToken = jwt.verify(token,JWT_SECRET);
+       const userId = decodedToken.userId;
+       if(req.param.id!=userId){
+          return res.status(403).json({
+            message:'Access Denied',
+            status:'error'
+          })
+       }
+       next();
     } catch (err) {
         return res.status(401).json({ message: 'Authentication failed: Invalid token.', status: 'error' });
     }
